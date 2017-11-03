a8f5016b09aaad45a6a5944bde18b9ff60956e6a
Alexsander Falcucci <alex.falcucci@gmail.com>
2017-11-03 00:54:38 -0200

    Does this work

--- problems/003.js
+++ problems/003.js
@@ -15,11 +15,38 @@ var utils = require('../utils/reusables.js');
 
 
 function is_prime(n) {
+  if (n == 2) {
+    return true
+  }
+  if (n == 3) {
+    return true
+  }
+  if (n % 2 == 0) {
+    return false
+  }
+  if (n % 3 == 0) {
+    return false
+  }
+
+  var i = 5;
+  var w = 2;
+
+  while (i * i <= n) {
+    if (n % i == 0) {
+      return false
+    }
+
+    i += w
+    w = 6 - w
+
+  }
+
   return true
 }
 
-function prime(n) {
+
+function factors(n) {
   return n;
 }
 
-utils.show_answer('03', prime(600851475143));
+utils.show_answer('03', factors(600851475143));
