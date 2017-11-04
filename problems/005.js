c1dd75c56546bd5060353cc9e1a54e4cf611fd7c
Alexsander Falcucci <alex.falcucci@gmail.com>
2017-11-04 17:04:05 -0200

    Whatever will be, will be 8{

---
+++ problems/005.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * Project Euler Problem 5
+ * =======================
+ *
+ * 2520 is the smallest number that can be divided by each of the numbers
+ * from 1 to 10 without any remainder.
+ *
+ * What is the smallest number that is evenly divisible by all of the numbers
+ * from 1 to 20?
+ *
+ */
+var utils = require('../utils/reusables.js');
+
+function main() {
+  return true;
+}
+
+utils.show_answer('05', main());
