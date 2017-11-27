c868c258fd1afde0bb027d32c4f53ad1db527f43
Alexsander Falcucci <alex.falcucci@gmail.com>
2017-11-27 19:33:56 -0200

    Trying to fake a conflict

---
+++ problems/010.js
@@ -0,0 +1,19 @@
+/**
+ *
+ *
+ * Project Euler Problem 10
+ * ========================
+ *
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ *
+ */
+
+var utils = require('../utils/reusables.js');
+
+function foo() {
+  return true;
+}
+
+utils.show_answer('10', foo());
